0a5b02eb34ba0aa3277cc5e12c5464c6a024f61c
viveksinra <vivek.kr.vks@gmail.com>
2025-04-22 12:56:20 +0530

    Enhance Home component by adding new sections: Content1, Process1, Process2, Process3, Cta1, and Cta2 for improved layout and functionality.

--- app/Home.js
+++ app/Home.js
@@ -1,5 +1,11 @@
 import Layout from "@/components/layout/Layout";
 import Banner1 from "@/components/sections/Banner1";
+import Content1 from "@/components/sections/Content1";
+import Cta1 from "@/components/sections/Cta1";
+import Cta2 from "@/components/sections/Cta2";
+import Process1 from "@/components/sections/Process1";
+import Process2 from "@/components/sections/Process2";
+import Process3 from "@/components/sections/Process3";
 // import Service4 from "@/components/sections/Service4";
 import ServiceStyle6 from "@/myComponent/ServiceStyle6";
 
@@ -10,6 +16,11 @@ export default function Home() {
     <Layout headerStyle={4} footerStyle={2}>
         <Banner1 />
         <ServiceStyle6 />
+        <Content1 />
+        <Process3 />
+        <Process2 />
+        <Cta2 />
+
     </Layout>
     </>
   );
