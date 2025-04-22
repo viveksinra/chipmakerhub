e8e5e12f62bedebfba6384f85a831c61b9e63414
viveksinra <vivek.kr.vks@gmail.com>
2025-04-22 12:04:21 +0530

    first commit

---
+++ app/Home.js
@@ -0,0 +1,13 @@
+import Layout from "@/components/layout/Layout";
+import Banner1 from "@/components/sections/Banner1";
+
+
+export default function Home() {
+  return (
+    <>
+    <Layout headerStyle={4} footerStyle={4}>
+        <Banner1 />
+    </Layout>
+    </>
+  );
+}
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,23 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import 'swiper/css'
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+import "../public/assets/css/plugins/animate.min.css"
+import "../public/assets/css/plugins/bootstrap.min.css"
+import "../public/assets/css/plugins/jquery.fancybox.min.css"
+import "../public/assets/css/plugins/owl.css"
+import "../public/assets/css/plugins/rangeslider.css"
+import "../public/assets/css/plugins/select.min.css"
+import "../public/assets/css/plugins/slick.css"
+
+import "../public/assets/css/global-settings.css"
+import "../public/assets/css/theme.css"
+
+import "../public/assets/css/plugins/flaticon_vankine.css"
+import "../public/assets/css/plugins/font-awesome.min.css"
+import "../public/assets/css/plugins/uicons-regular-rounded.css"
+import "../public/assets/css/plugins/uicons-regular-straight.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
