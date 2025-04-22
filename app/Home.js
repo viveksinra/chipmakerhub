ececf1d744e456bc75079db89c72b92f04bcb7c9
viveksinra <vivek.kr.vks@gmail.com>
2025-04-22 12:47:28 +0530

    Update package dependencies, enhance layout styles, and refactor main page component. Added new CSS imports for improved styling and removed unused SVG files.

--- app/Home.js
+++ app/Home.js
@@ -1,12 +1,15 @@
 import Layout from "@/components/layout/Layout";
 import Banner1 from "@/components/sections/Banner1";
+// import Service4 from "@/components/sections/Service4";
+import ServiceStyle6 from "@/myComponent/ServiceStyle6";
 
 
 export default function Home() {
   return (
     <>
-    <Layout headerStyle={4} footerStyle={4}>
+    <Layout headerStyle={4} footerStyle={2}>
         <Banner1 />
+        <ServiceStyle6 />
     </Layout>
     </>
   );
